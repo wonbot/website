92f3e79b38b7958813e80bad7e8ab36e8636be19
nxyy <nxyy@nxyy.store>
2025-08-08 11:30:26 -0400

    f

--- src/app/h/route.ts
+++ src/app/h/route.ts
@@ -1,5 +1,5 @@
 export async function GET(request: Request) {
-  const referer = request.headers.get('referer');
+  const referer = request.headers.get("referer");
 
   if (!referer) {
     return Response.redirect("https://expel.best");
@@ -9,7 +9,7 @@ export async function GET(request: Request) {
     headers: {
       Authorization: "Bearer NxyyS3cr3tK3y!!!?",
     },
-    cache: "no-store"
+    cache: "no-store",
   });
 
   const data = await res.json();
--- src/app/s/route.ts
+++ src/app/s/route.ts
@@ -1,18 +1,18 @@
 export async function GET(request: Request) {
-    const referer = request.headers.get('referer');
-    
-    if (!referer) {
-        return Response.redirect("https://expel.best");
-    }
-    
-    const res = await fetch("https://api.expel.best/bot/status", {
-        method: "GET",
-        headers: {
-            Authorization: "Bearer NxyyS3cr3tK3y!!!?",
-        },
-        cache: 'no-store'
-    });
+  const referer = request.headers.get("referer");
 
-    const data = await res.json();
-    return Response.json(data);
+  if (!referer) {
+    return Response.redirect("https://expel.best");
+  }
+
+  const res = await fetch("https://api.expel.best/bot/status", {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer NxyyS3cr3tK3y!!!?",
+    },
+    cache: "no-store",
+  });
+
+  const data = await res.json();
+  return Response.json(data);
 }
